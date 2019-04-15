06a890f74d2451707bc0b9e232aa2008754d734a
Guilherme Galarça Dutra <udutra@gmail.com>
2019-04-15 11:44:00 -0300

    Ajustes Iniciais.
    Criação do cenário.
    Criação do Player.
    Criação do inimigo
    Criação dos Scripts: Combo, Hit, PlayerCombo.

---
+++ Assets/Scripts/Hit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Hit
+{
+    public string animation, inputbutton;
+    public float animationTime, resetTime;
+    public int damage;
+    public AudioClip hitSound;
+    public bool slowDown;
+}
---
+++ Assets/Scripts/PlayerCombo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class PlayerCombo : MonoBehaviour
+{
+    public Combo[] combos;
+}
