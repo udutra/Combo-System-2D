0e519232c1b82a1f06bbf5810f2a22fb7dca09f8
Guilherme Galarça Dutra <udutra@gmail.com>
2019-04-25 11:09:06 -0300

    Recebendo dano.
    Criação do script Damage.
    Criação do Canvas e do Texto de dano.
    Criação do Prefab do texto de dano.
    Correção das animações.
    Aleteração no script PlayerCombo e Attack.

---
+++ Assets/Scripts/Damage.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Damage : MonoBehaviour
+{
+    public int life;
+    public Color defaultColor, damageColor;
+    public float damageTime;
+    public GameObject damageText;
+    public Transform damageTextPosition;
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        defaultColor = spriteRenderer.color;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        life -= damage;
+        spriteRenderer.color = damageColor;
+        Invoke("ReleaseDamage", damageTime);
+        GameObject newDamageText = Instantiate(damageText, damageTextPosition.position, Quaternion.identity);
+        newDamageText.GetComponentInChildren<Text>().text = damage.ToString();
+        Destroy(newDamageText, 1);
+
+    }
+
+    private void ReleaseDamage()
+    {
+        spriteRenderer.color = defaultColor;
+    }
+}
